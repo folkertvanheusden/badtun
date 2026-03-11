0a6184589613b19997f8fdd3051f1e423835f60c
Folkert van Heusden <folkert@vanheusden.com>
2026-03-11 16:54:44 +0100

    configurable via commandline

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ This is VPN software (like openvpn, wireguard, ipsec, etc).
 Do not use.
 
 
+
 Folkert van Heusden <folkert@komputilo.nl>
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <cstring>
+#include <getopt.h>
 #include <poll.h>
 #include <unistd.h>
 #include <arpa/inet.h>
@@ -9,68 +10,114 @@
 #include "net.h"
 
 
+void help()
+{
+	fprintf(stderr, "badtun " VERSION "\n");
+	fprintf(stderr, "-p  password\n");
+	fprintf(stderr, "-n  interface name\n");
+	fprintf(stderr, "-P  server UDP port via which the vpn talks\n");
+	fprintf(stderr, "-r  IP address of peer\n");
+	fprintf(stderr, "-m  mode: \"client\" or \"server\"\n");
+}
+
 int main(int argc, char *argv[])
 {
-	std::string psk            = "Dit is een test!";  // you may want to change this
-	auto        tun_parameters = open_tun("badtun");
+	std::string psk            = "Dit is een test!";  // you may want to change this (use -p)
+	std::string interface_name = "badtun";
+	int         local_port     = 4100;
+	std::string remote_addr    = "94.142.246.161";
+	bool        is_server      = true;
+
+	int c = -1;
+	while((c = getopt(argc, argv, "p:n:P:r:m:h")) != -1) {
+		if (c == 'p')
+			psk = optarg;
+		else if (c == 'n')
+			interface_name = optarg;
+		else if (c == 'P')
+			local_port = std::stoi(optarg);
+		else if (c == 'r')
+			remote_addr = optarg;
+		else if (c == 'm')
+			is_server = strcmp(optarg, "server") == 0;
+		else if (c == 'h') {
+			help();
+			return 0;
+		}
+		else {
+			help();
+			return 127;
+		}
+	}
+
+	auto   tun_parameters = open_tun(interface_name);
 	if (tun_parameters.has_value() == false)
 		return 1;
-	int         udp_fd         = listen_on_udp_port(4100);
+	int    udp_fd         = listen_on_udp_port(local_port);
 	if (udp_fd == -1)
 		return 2;
-	pollfd      fds[]          = { { tun_parameters.value().fd, POLLIN, 0 }, { udp_fd, POLLIN, 0 } };
+	pollfd fds[]          = { { tun_parameters.value().fd, POLLIN, 0 }, { udp_fd, POLLIN, 0 } };
 
 	sockaddr_in target { };
         target.sin_family = AF_INET;
-        target.sin_port   = htons(4100);
-	inet_aton("94.142.246.161", &target.sin_addr);  // this also won't make any sense
+        target.sin_port   = htons(local_port);  // will be overwritten when a client packet is received (in server mode)
+	inet_aton(remote_addr.c_str(), &target.sin_addr);
 
-	unsigned char key[SHA256_DIGEST_LENGTH] { };
+	constexpr const int key_size = SHA256_DIGEST_LENGTH;
+
+	unsigned char key[SHA256_DIGEST_LENGTH] { };  // 32 bytes
 	SHA256_CTX sha256;
 	SHA256_Init(&sha256);
 	SHA256_Update(&sha256, psk.data(), psk.size());
 	SHA256_Final(key, &sha256);
 
         AES_KEY aes_key_e;
-	AES_set_encrypt_key(key, SHA256_DIGEST_LENGTH * 8, &aes_key_e);
+	AES_set_encrypt_key(key, key_size * 8, &aes_key_e);  // 256 bits
         AES_KEY aes_key_d;
-	AES_set_decrypt_key(key, SHA256_DIGEST_LENGTH * 8, &aes_key_d);
+	AES_set_decrypt_key(key, key_size * 8, &aes_key_d);
+
+	sockaddr_in peer_addr     {   };
+	socklen_t   peer_addr_len { 0 };
 
 	for(;;) {
 		int rc = poll(fds, 2, -1);
-		if (rc == 0)
-			continue;
-		if (rc == -1)
-			break;
+		if (rc <= 0)
+			return 3;
 
 		if (fds[0].revents) {
-			uint8_t ivec[16]         { };
+			uint8_t ivec[key_size]   { };
 			uint8_t buffer_in [1600] { };
 			uint8_t buffer_out[1600] { };
 			int     rc     = read(tun_parameters.value().fd, &buffer_in[2], sizeof(buffer_in) - 2);
 			if (rc == -1)
-				break;
+				return 4;
 
 			buffer_in[0] = rc >> 8;
 			buffer_in[1] = rc;
 
-			for(size_t o=0; o<rc + 2; o += 16)
-				AES_cbc_encrypt(&buffer_in[o], &buffer_out[o], 16, &aes_key_e, ivec, AES_ENCRYPT);
+			for(size_t o=0; o<rc + 2; o += key_size)
+				AES_cbc_encrypt(&buffer_in[o], &buffer_out[o], key_size, &aes_key_e, ivec, AES_ENCRYPT);
 
-			sendto(udp_fd, buffer_out, (rc + 15) & ~15, 0, reinterpret_cast<const sockaddr *>(&target), sizeof target);
+			rc = (rc + key_size - 1) & ~(key_size - 1);
+			if (is_server)
+				sendto(udp_fd, buffer_out, rc, 0, reinterpret_cast<const sockaddr *>(&peer_addr), sizeof peer_addr_len);
+			else
+				sendto(udp_fd, buffer_out, rc, 0, reinterpret_cast<const sockaddr *>(&target   ), sizeof target       );
 		}
 
 		if (fds[1].revents) {
-			uint8_t ivec[16]         { };
+			uint8_t ivec[key_size]   { };
 			uint8_t buffer_in [1600] { };
 			uint8_t buffer_out[1600] { };
-			int     rc       = recv(udp_fd, buffer_in, sizeof buffer_in, 0);
+			int     rc       = is_server == false ? recv    (udp_fd, buffer_in, sizeof buffer_in, 0) :
+       								recvfrom(udp_fd, buffer_in, sizeof buffer_in, 0,
+										reinterpret_cast<sockaddr *>(&peer_addr), &peer_addr_len);
 			if (rc == -1)
-				break;
-			if ((rc & ~15) != rc)
+				return 5;
+			if ((rc & ~(key_size - 1)) != rc)
 				continue;
-			for(size_t o=0; o<rc; o += 16)
-				AES_cbc_encrypt(&buffer_in[o], &buffer_out[o], 16, &aes_key_d, ivec, AES_DECRYPT);
+			for(size_t o=0; o<rc; o += key_size)
+				AES_cbc_encrypt(&buffer_in[o], &buffer_out[o], key_size, &aes_key_d, ivec, AES_DECRYPT);
 
 			size_t  real_len = (buffer_out[0] << 8) | buffer_out[1];
 			if (real_len > sizeof buffer_out - 2)
@@ -82,4 +129,6 @@ int main(int argc, char *argv[])
 	close(udp_fd);
 
 	close(tun_parameters.value().fd);
+
+	return 0;
 }
